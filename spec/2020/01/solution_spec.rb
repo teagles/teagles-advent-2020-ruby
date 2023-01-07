c2c92a181744ce8e8a93e4a48b63fad5efdb78fc
Tim Eagles <tim.eagles.au@gmail.com>
2023-01-06 20:39:41 -0800

    2020-01-1: Starting state.

---
+++ spec/2020/01/solution_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Year2020::Day01 do
+  let(:input) { File.read(File.join(File.dirname(__FILE__), '../../../challenges/2020/01/input.txt')) }
+  let(:example_input) do
+    <<~EXAMPLE_INPUT
+      change
+      me
+    EXAMPLE_INPUT
+  end
+
+  describe 'part 1' do
+    it 'returns 514579 for the example input' do
+      expect(described_class.part_1(example_input)).to eq(514579)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_1(input)).to eq(nil)
+    end
+  end
+
+  describe 'part 2' do
+    it 'returns nil for the example input' do
+      expect(described_class.part_2(example_input)).to eq(nil)
+    end
+
+    it 'returns nil for my input' do
+      expect(described_class.part_2(input)).to eq(nil)
+    end
+  end
+end
